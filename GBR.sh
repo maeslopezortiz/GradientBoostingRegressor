ead758e01f97e2b39fcda8f705a554ca7b3554ef
María Estefanía López Ortiz <148332361+maeslopezortiz@users.noreply.github.com>
2024-02-15 10:13:08 +0100

    GBR.sh

--- GBR.sh
+++ GBR.sh
@@ -14,7 +14,7 @@
 #multiple species
  #for i in "${!SPECIES[@]}"; do sbatch GBR.sh "${SPECIES[i]}" "${TRAIT[i]}"; done
 # Make sure to load the appropriate Python module, e.g., module load python/3.7.4 if you are in euler
-
+# Download the EasyGeSe folder from the repository 
 #module load python/3.7.4
 #module load gcc/6.3.0 r/4.1.3
 #git clone https://github.com/stevenandrewyates/EasyGeSe
@@ -23,7 +23,7 @@ SPECIES=$1
 TRAIT=$2
 
 # This file contains species names and the number of traits
-mkdir GBR_results
+mkdir GBR_results #generate the directory where the results will go.
 
 python_success=true
 
@@ -31,7 +31,7 @@ for TRAIT in $(seq 1 $TRAIT); do
    for CV in {1..5}; do
       for FOLD in {1..2}; do
          echo "Running with arguments: $SPECIES, TRAIT=$TRAIT, CV=$CV, FOLD=$FOLD"
-         python GBR.py "$SPECIES" "$TRAIT" "$CV" "$FOLD" # To execute your Python script
+         python GBR_na.py "$SPECIES" "$TRAIT" "$CV" "$FOLD" # To execute your Python script. It can be modified to run for testing estimators or number of samples
          python_exit_status=$? # Capture the exit status of the Python script
 
          if [ $python_exit_status -ne 0 ]; then
